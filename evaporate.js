a34cb8c5b01416aef74384c9d78904e154708ffd
bikeath1337 <bikeath1337@me.com>
2016-09-01 17:43:23 -0400

    Restores reset of part status.

--- evaporate.js
+++ evaporate.js
@@ -702,6 +702,8 @@
                         addPartToProcessing(part);
                         authorizedSend(upload);
                         l.d('upload #', partNumber, upload);
+                    } else {
+                        part.status = PENDING;
                     }
                 }, backOff);
 
